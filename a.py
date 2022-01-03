d3668200f9dd89aff754ff05c24c7a385e3a51ce
AriyaISHIHARA <ariya.ishihara@groove-x.com>
2022-01-04 08:43:42 +0900

    latex format, cl opt.

--- a.py
+++ a.py
@@ -1,3 +1,10 @@
+import sys
+
+
+def _cubesumtex(items):
+    return " + ".join(f"{i}^3" if i > 0 else f"({i})^3" for i in items)
+
+
 def _q(already, new, updates, elems, new_elem):
     if new > 0:
         if new not in already:
@@ -7,7 +14,7 @@ def _q(already, new, updates, elems, new_elem):
             updates[-new] = tuple(-e for e in (*elems, new_elem))
 
 
-def _main():
+def _search(sup):
     res = {0: ()}
     n = 1
     wanted = 1
@@ -19,11 +26,21 @@ def _main():
             _q(res, i - c, updates, breakdown, -n)
         res.update(updates)
         while wanted in res:
-            print(f"{wanted}: {res[wanted]}")
+            print(rf"\\{wanted} &=& {_cubesumtex(res[wanted])}")
             wanted += 1
+            if wanted == sup + 1:
+                return
         n += 1
 
 
-if __name__ == '__main__':
-    _main()
+def _main(sup=-1):
+    print("[math]\\displaystyle\\begin{eqnarray}\n0 &=& 1+3 + (-1)^3")
+    _search(sup)
+    print(r"\end{eqnarray}[/math]")
 
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        _main(int(sys.argv[1]))
+    else:
+        _main()
